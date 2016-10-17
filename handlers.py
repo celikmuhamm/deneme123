a7696a0a738db68317cc5e901599fe1a9a54db2c
cicekn <cicekn@itu.edu.tr>
2016-10-17 21:10:04 +0300

    Şablon Güncelleme - Edit Project Template
    Handlers.py dosyası sitede kullanılacak sayfaların fonksiyonlarını
    içerir. Settings.py dosyası genel ayarları içerir.

---
+++ handlers.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, render_template
+from flask import current_app
+
+from datetime import datetime
+
+
+site = Blueprint('site', __name__)
+
+
+@site.route('/')
+def home_page():
+    now = datetime.now()
+    day = now.strftime('%A')
+    return render_template('home.html', day_name=day)
+
+
+@site.route('/events')
+def events_page():
+    events = current_app.store.get_events()
+    now = datetime.now()
+    day = now.strftime('%A')
+    return render_template('events.html', events=sorted(events.items()),day_name=day )
+
+@site.route('/events/documents')
+def documents_page():
+    events = current_app.store.get_events()
+    return render_template('documents.html', events=sorted(events.items()))
+
+
