f508c1c839d9cf3fea81cc8284919e794ffcc3a5
celikmuhamm <safa@safa-MS-7978>
2016-10-18 00:53:01 +0300

    server.py correction

--- server.py
+++ server.py
@@ -10,19 +10,23 @@ from store import Store
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('settings')
-	app.register_blueprint(site)
-	
-	app.store = Store()
+    app.register_blueprint(site)
+    app.store = Store()
     app.store.add_event(Event('World War II', date='15/12/1942', place='Turkey',content= 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit'))
     app.store.add_event(Event('Train Accident', date='01/02/1985', place='California', content = 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit'))
-   
     return app
 
 def main():
     app = create_app()
-    app.run(host='0.0.0.0', port=5000)
-
+    VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
+    if VCAP_APP_PORT is not None:
+        port, debug = int(VCAP_APP_PORT), False
+    else:
+        port, debug = 5000, True
+    app.run(host='0.0.0.0', port=port, debug=debug)
 
 if __name__ == '__main__':
     main()
+
+
+
