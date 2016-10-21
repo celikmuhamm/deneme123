e5d0dacf11ab382391750ac3604dfe41f4b5c75f
Safa Çelik <safacelik13@gmail.com>
2016-10-22 02:42:31 +0300

    try to run2

--- vagrant/start-flask.sh
+++ vagrant/start-flask.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 vagrant ssh -c "cd /vagrant && sudo pip3 install -U -r requirements.txt && python3 server.py"
+vagrant ssh -c "cd /vagrant && sudo pip3 install --upgrade pip3 && python3 server.py"
