d012608f94dc091c4fccf8f0a547b869f8ebc6a8
Safa Çelik <safacelik13@gmail.com>
2016-10-22 02:38:24 +0300

    try to run on server

--- db2/installdb2.sh
+++ db2/installdb2.sh
@@ -2,7 +2,6 @@
 
 tar xzf /vagrant/*_expc.tar.gz
 sudo ./expc/db2setup -r /vagrant/db2expc.rsp
-sudo pip install --upgrade pip
 rm -rf expc
 sudo addgroup vagrant db2iadm1
 sudo chmod 755 /opt/ibm/db2/V10.5/adm/db2start
