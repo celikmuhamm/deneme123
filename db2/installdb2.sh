e3042ea6fb46e4a95e8c5d34c74f5c7d3542aa74
Safa Çelik <safacelik13@gmail.com>
2016-10-22 02:34:23 +0300

    trying to upgrade pip

--- db2/installdb2.sh
+++ db2/installdb2.sh
@@ -2,6 +2,7 @@
 
 tar xzf /vagrant/*_expc.tar.gz
 sudo ./expc/db2setup -r /vagrant/db2expc.rsp
+sudo pip install --upgrade pip
 rm -rf expc
 sudo addgroup vagrant db2iadm1
 sudo chmod 755 /opt/ibm/db2/V10.5/adm/db2start
