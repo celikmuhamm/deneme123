b3638d13a6184d616707d5fd976da467e38547b8
onushalil <onusha@itu.edu.tr>
2016-10-30 01:02:22 +0300

    database connection sqlconnection.py
     basic timeline table(TIMETABLE) added to the
    project, connections with map will be done later.

--- sqlconnection.py
+++ sqlconnection.py
@@ -18,9 +18,18 @@ def initialize_database():
     connection = getConnection();
     cursor = connection.cursor()
     """cursor.execute(DROP TABLE IF  EXISTS USERTABLE )"""
+    
+    """cursor.execute(DROP TABLE IF  EXISTS TIMETABLE )"""
     try:
        cursor.execute("""CREATE TABLE IF NOT EXISTS USERTABLE (username varchar(20),password varchar(20), email varchar(40),name varchar(20),surname varchar(20))""")
-       connection.commit()
+       
+       cursor.execute("""CREATE TABLE IF NOT EXISTS TIMETABLE (map_id varchar(40) primary key, decade int not null,year int not null,share_date date not null,content_type varchar(40),content_header  varchar(40))""")
+       
+       cursor.execute("""INSERT INTO TIMETABLE (map_id,decade,year,share_date,content_type,content_header) VALUES ('1',1960,1963,'1963-08-22','text','X-15 aircraft')""" )  
+       
+       cursor.execute("""INSERT INTO TIMETABLE (map_id,decade,year,share_date,content_type,content_header) VALUES ('2',2010,2016,'1963-06-03','text','Mohammed Morsi')""" )  
+        
+        connection.commit()
     except connection.Error as error:
             print(error)
    
