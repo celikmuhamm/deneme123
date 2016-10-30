37ee5d2377b899743d01c7e3feb71de6a070fbe9
cicekn <cicekn@itu.edu.tr>
2016-10-31 02:22:27 +0300

    Tablo oluşturmak amacıyla init_db fonksiyonu eklendi
    Veritabanına bağlanmak ve tablo oluşturmak amacıyla init_db fonksiyonu
    eklendi. Halil ve Safa'nın önceki initialize_database fonksiyonunda
    yazmış oldukları tablo oluşturma ve tabloya ekleme komutları kopyalandı.
    Olaylar 'events' için tablo oluşturuldu.

---
+++ initialize_database.py
@@ -0,0 +1,38 @@
+import os
+import json
+import re
+import psycopg2 as dbapi2
+from handlers import site
+from sql_connection_for_events import get_connection_for_events
+
+def init_db():
+    dsn = get_connection_for_events()
+    with dbapi2.connect(dsn) as connection:
+        cursor = connection.cursor()
+            
+        """cursor.execute(DROP TABLE IF  EXISTS USERTABLE )"""    
+            
+        query = """DROP TABLE IF  EXISTS EVENTTABLE"""
+        cursor.execute(query)
+    
+        cursor.execute("""DROP TABLE IF  EXISTS TIMETABLE """)
+   
+    try:
+        cursor.execute("""CREATE TABLE IF NOT EXISTS EVENTTABLE (title varchar(30), date varchar(10), place varchar(40))""")
+
+        cursor.execute("""CREATE TABLE IF NOT EXISTS USERTABLE (username varchar(20),password varchar(20), email varchar(40),name varchar(20),surname varchar(20))""")
+     
+        cursor.execute("""CREATE TABLE IF NOT EXISTS TIMETABLE (map_id varchar(40) primary key, decade int not null,year int not null,share_date date not null,content_type varchar(40),content_header  varchar(40))""")
+       
+        cursor.execute("""INSERT INTO TIMETABLE (map_id,decade,year,share_date,content_type,content_header) VALUES ('1',1960,1963,'1963-08-22','text','X-15 aircraft')""" )  
+       
+        cursor.execute("""INSERT INTO TIMETABLE (map_id,decade,year,share_date,content_type,content_header) VALUES ('2',2010,2016,'1963-06-03','text','Mohammed Morsi')""" )  
+       
+        connection.commit()
+    except connection.Error as error:
+            print(error)
+           
+    connection.close()
+
+
+
