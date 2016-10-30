6d3bc520e0f0fe499670e7f456e8c30cd7a68ff5
cicekn <cicekn@itu.edu.tr>
2016-10-31 02:16:47 +0300

    Olaylar "Events" için SQL bağlantısı yapıldı
    Olaylar için veritabanını başlatmada gerekli olan SQL bağlantı
    fonksiyonları (get_sqldb_dsn ve get_connection_for_events) eklendi

---
+++ sql_connection_for_events.py
@@ -0,0 +1,26 @@
+import os
+import json
+import re
+import psycopg2 as dbapi2
+from handlers import site
+
+
+def get_sqldb_dsn(vcap_services):
+    """Returns the data source name for IBM SQL DB."""
+    parsed = json.loads(vcap_services)
+    uri = parsed["elephantsql"][0]["credentials"]["uri"]
+    match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)',uri)
+    user,password,host, _,port,dbname = match.groups()
+    dsn = """user='{}' password='{}' host='{}' port={} dbname='{}'""".format(user, password, host, port, dbname)
+    return dsn
+
+
+def get_connection_for_events():
+    VCAP_SERVICES = os.getenv('VCAP_SERVICES')
+    if VCAP_SERVICES is not None:
+        dsn = get_sqldb_dsn(VCAP_SERVICES)
+    else:
+        dsn = """user='vagrant' password='vagrant'
+                               host='localhost' port=5432 dbname='itucsdb'"""
+    
+    return dsn;
