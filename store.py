a7490ac1e7a671c90ce878ac3860ed6752532b30
cicekn <cicekn@itu.edu.tr>
2016-10-31 02:20:19 +0300

    "add_event" fonksiyonuna INSERT INTO komutu eklendi
    "add_event" fonksiyonunun daha önce oluşturulmuş tabloya veri
    yazabilmesi için gerekli veritabanı bağlantısı kuruldu ve INSERT INTO
    komutu çalıştırıldı

--- store.py
+++ store.py
@@ -3,6 +3,12 @@ Created on 16 Eki 2016
 
 @author: mert
 '''
+import os
+import json
+import re
+import psycopg2 as dbapi2
+from sql_connection_for_events import get_connection_for_events
+
 class Store:
     def __init__(self):
         self.events = {}
@@ -12,6 +18,20 @@ class Store:
         self.last_event_id += 1
         self.events[self.last_event_id] = event
         event._id = self.last_event_id
+        dsn = get_connection_for_events();
+        with dbapi2.connect(dsn) as connection:
+            cursor = connection.cursor()
+            title = event.title
+            date = event.date
+            place = event.place
+            query = """INSERT INTO EVENTTABLE (title,date,place) VALUES (%s, %s, %s)"""
+        try: 
+            cursor.execute(query, (event.title, event.date, event.place))
+            connection.commit()
+            self.last_key = cursor.lastrowid
+        except connection.Error as error:
+            print(error)
+        connection.close()
 
     def delete_event(self, event_id):
         del self.events[event_id]
