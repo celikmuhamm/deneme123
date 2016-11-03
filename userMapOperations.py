20af14d876bbace778439e494af9d55c41563aa7
oyku <oyku@oyku-N56VZ>
2016-11-03 14:26:09 +0200

    bug fixed

--- userMapOperations.py
+++ userMapOperations.py
@@ -10,7 +10,7 @@ myMap = Blueprint('myMap', __name__)
 @myMap.route('/userPage/map',methods=['POST','GET'])
 def insertFunction():
     if request.method == 'GET':
-        return render_template('user_page.html')
+        return render_template('user_page.html',userMap = current_app.usermap.myLocations,user_name = current_app.user.username,first_name=current_app.user.name,last_name = current_app.user.surname,e_mail=current_app.user.email)
     else:
          if request.method == 'POST':
              userName = current_app.user.username
@@ -20,10 +20,12 @@ def insertFunction():
              lng = request.form['lng']
              if current_app.userlocation.mapInfo == mapInfo:
                  return render_template('user_page.html',userMap = current_app.usermap.myLocations,user_name = current_app.user.username,first_name=current_app.user.name,last_name = current_app.user.surname,e_mail=current_app.user.email)
-             current_app.userlocation.userName = userName
-             current_app.userlocation.mapInfo = mapInfo
-             current_app.userlocation.address = address
-             current_app.userlocation.lat = lat
-             current_app.userlocation.lng = lng
-             current_app.usermap.addLocation(current_app.userlocation)
-             return render_template('user_page.html',userMap = current_app.usermap.myLocations,user_name = current_app.user.username,first_name=current_app.user.name,last_name = current_app.user.surname,e_mail=current_app.user.email)
+             else:
+
+                 current_app.userlocation.userName = userName
+                 current_app.userlocation.mapInfo = mapInfo
+                 current_app.userlocation.address = address
+                 current_app.userlocation.lat = lat
+                 current_app.userlocation.lng = lng
+                 current_app.usermap.addLocation(current_app.userlocation)
+                 return render_template('user_page.html',userMap = current_app.usermap.myLocations,user_name = current_app.user.username,first_name=current_app.user.name,last_name = current_app.user.surname,e_mail=current_app.user.email)
