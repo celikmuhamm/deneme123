23251de035b751e1f4805feee8bf24362118cfc0
cicekn <cicekn@itu.edu.tr>
2016-11-29 08:09:59 +0300

    Image and Document classes are added

--- event.py
+++ event.py
@@ -4,8 +4,25 @@ Created on 16 Eki 2016
 @author: mert
 '''
 class Event:
-    def __init__(self, title, content, date=None, place=None):
+    def __init__(self, title, event_id, date=None, place=None):
         self.title = title
         self.date = date
-        self.content = content
         self.place = place
+        self.event_id=event_id
+        
+        
+class Image:
+    def __init__(self, event_id, image_id, content, date=None):
+        self.event_id = event_id
+        self.image_id = image_id
+        self.date = date
+        self.content = content
+        
+        
+class Document:
+    def __init__(self, event_id, document_id, content, title, date=None):
+        self.event_id = event_id
+        self.document_id = document_id
+        self.date = date
+        self.content = content
+        self.title = title
