cdd2ed21caa7d6969cca8da48545ab507e5c47e7
cicekn <cicekn@itu.edu.tr>
2016-11-29 11:10:38 +0200

    update handlers.py
    page handler functions for events, documents and images are added

--- handlers.py
+++ handlers.py
@@ -16,15 +16,52 @@ def home_page():
 
 @site.route('/events')
 def events_page():
-    total_number = current_app.store.last_event_id
-    return render_template('events.html',tot_num =total_number )
+    return render_template('events.html')
 
-@site.route('/events/documents', methods=['GET', 'POST'])
+@site.route('/events/documents_all/<int:event_id>', methods=['GET', 'POST'])
+def documents_all_page(event_id):
+    documents_array = current_app.store_documents.get_documents(event_id)
+    form = {'inputTitle': '', 'inputDate': '', 'event_number': '', 'comment':''}
+ 
+    return render_template('documents.html', event_id=event_id, documents=documents_array, form=form)
+
+@site.route('/events/events_list', methods=['GET', 'POST'])
 def documents_page():
     form = {'inputTitle': '', 'inputDate': '', 'inputPlace': '', 'comment':''}
-    events = current_app.store.get_events()
-    return render_template('documents.html', events=events, form=form)
+    events_array = current_app.store.get_events()
+    
+    return render_template('events_list.html', events=events_array, form=form)
 
+@site.route('/events/images')
+def images_page():
+    counter = 0
+    events_array = current_app.store.get_events()
+    image_array = None
+    for events in events_array:
+        event_id = events.event_id
+        image_series = current_app.store_images.get_images(event_id)
+        images = [(image_series)]
+        if image_series:
+            if(event_id == 1):
+                image_array = images
+            else:
+                image_array += images
+        else:
+            counter += 1
+    total_count = current_app.store.get_total_events()
+    count=total_count[0]-counter
+    
+    return render_template('images_slide.html', count=count, images=image_array)
+
+@site.route('/events/images/images_add')
+def images_add_page():
+    events = current_app.store.get_events()
+    return render_template('add_images.html', events=events)
+
+@site.route('/events/images_all/<int:event_id>')
+def images_all_page(event_id):
+    images = current_app.store_images.get_images(event_id)
+    return render_template('images_all.html',images = images, event_id = event_id)
 
 @site.route('/signUp')
 def sign_up():
